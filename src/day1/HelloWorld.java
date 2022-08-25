3ea1868eedb80bc3aa5a54cf03cb39ce121013c4
aysunyigit <camciaysun@hotmail.com>
2022-08-25 14:21:33 -0500

    added some changes on HelloWorld class.

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,6 +4,12 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Good old days");
+        int year = 2010;
+        String schoolName = "Suleymaniye Kiz Kursu";
+        System.out.println("Year; " + year + "\nSchool Name; " + schoolName);
+
+
+
 
 
     }
